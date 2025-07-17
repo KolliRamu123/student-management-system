254b3e59c663e058e1bcf1e38d88f4e603be336d
KolliRamu <kolliramu20001@gmail.com>
2025-07-17 19:29:01 +0530

    added changes

--- src/main/java/com/sms/entity/repository/StudentRepository.java
+++ src/main/java/com/sms/entity/repository/StudentRepository.java
@@ -1,18 +1,12 @@
 package com.sms.entity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-
-import com.sms.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student,Long>{
-=======
 import org.springframework.stereotype.Repository;
 
 import com.sms.entity.Student;
 
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
->>>>>>> ramu
 
 }
